71bc77861574587ec6bb9ec43bb2f4cd5c99b38f
Rubberbang <rubberbangg@gmail.com>
2025-02-11 21:17:03 +0100

    updates

--- scrtipt.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("mailButton").addEventListener("click", function() {
-    const truckContainer = document.getElementById("truckContainer");
-    truckContainer.style.left = "0"; // Move the truck across the screen
-});
