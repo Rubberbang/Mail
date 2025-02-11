0706356a070442f9550431bdbf02eb80581cb5af
Rubberbang <rubberbangg@gmail.com>
2025-02-11 20:58:16 +0100

    updates

---
+++ scrtipt.js
@@ -0,0 +1,4 @@
+document.getElementById("mailButton").addEventListener("click", function() {
+    const truckContainer = document.getElementById("truckContainer");
+    truckContainer.style.left = "0"; // Move the truck across the screen
+});
